2275f40f22ef792761869c5d51e24c263be2c119
Thomas Edstrom <thomasjedstrom@gmail.com>
2016-10-07 22:16:53 -0500

    added xframe fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,18 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+	protect_from_forgery with: :exception
 
-  use_vanity :user_identity
-  
-  protected
+	use_vanity :user_identity
 
-  def user_identity
-  	(0...8).map { (65 + rand(26)).chr }.join
-  end
+	after_filter :set_header_for_iframe
+
+	private
+	def set_header_for_iframe
+		response.headers.delete "X-Frame-Options" 
+	end
+
+	protected
+
+	def user_identity
+		(0...8).map { (65 + rand(26)).chr }.join
+	end
 end
